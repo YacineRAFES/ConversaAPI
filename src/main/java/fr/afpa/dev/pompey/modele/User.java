d70d1b9f12aa79d989c68300a8fe5caf90f7e40b
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-03-20 09:36:04 +0100

    Mis en place le code defensif

--- src/main/java/fr/afpa/dev/pompey/modele/User.java
+++ src/main/java/fr/afpa/dev/pompey/modele/User.java
@@ -1,10 +1,12 @@
 package fr.afpa.dev.pompey.modele;
 
+import fr.afpa.dev.pompey.utilitaires.Regex;
+import fr.afpa.dev.pompey.exception.SaisieException;
+
 import java.sql.Date;
-import java.time.LocalDate;
 
 public class User {
-    private int id;
+    private Integer id;
     private String name;
     private String password;
     private String email;
@@ -16,7 +18,7 @@ public class User {
 
     }
 
-    public User(int id, String name, String password, String email, String role, Date date) {
+    public User(Integer id, String name, String password, String email, String role, Date date) {
         this.id = id;
         this.name = name;
         this.password = password;
@@ -25,7 +27,7 @@ public class User {
         this.role = role;
     }
 
-    public User(int id){
+    public User(Integer id){
         this.id = id;
     }
 
@@ -35,7 +37,10 @@ public class User {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
+        if (id != null && id <= 0) {
+            throw new IllegalArgumentException("L'id ne doit pas être négatif");
+        }
         this.id = id;
     }
 
@@ -43,7 +48,12 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) throws SaisieException {
+        if (name == null || name.isEmpty()) {
+            throw new SaisieException("Le nom ne doit pas être vide");
+        } else if (!name.matches(Regex.REGEX_NAME)) {
+            throw new SaisieException("Le nom ne corresponds pas");
+        }
         this.name = name;
     }
 
@@ -51,7 +61,10 @@ public class User {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) throws SaisieException {
+        if (password == null || password.isEmpty()) {
+            throw new SaisieException("Le mot de passe ne doit pas être vide");
+        }
         this.password = password;
     }
 
@@ -59,7 +72,12 @@ public class User {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email) throws SaisieException {
+        if (email == null || email.isEmpty()) {
+            throw new SaisieException("L'email ne doit pas être vide");
+        } else if (!email.matches(Regex.REGEX_EMAIL)) {
+            throw new SaisieException("L'email ne corresponds pas");
+        }
         this.email = email;
     }
 
@@ -67,7 +85,10 @@ public class User {
         return role;
     }
 
-    public void setRole(String role) {
+    public void setRole(String role) throws SaisieException {
+        if (role == null || role.isEmpty()) {
+            throw new SaisieException("Le role ne doit pas être vide");
+        }
         this.role = role;
     }
 
@@ -75,7 +96,10 @@ public class User {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Date date) throws SaisieException {
+        if (date == null) {
+            throw new SaisieException("La date ne doit pas être null");
+        }
         this.date = date;
     }
 }
