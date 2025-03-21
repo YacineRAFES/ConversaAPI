1e7a41e6dd2078c6a588a2751bcd93775d025e81
Yacine RAFES <yacine.rafes@gmail.com>
2025-03-21 11:50:11 +0100

    Modification de la Sécurisation de mot de passe et ajout des JAVADOC

--- src/main/java/fr/afpa/dev/pompey/securite/Securite.java
+++ src/main/java/fr/afpa/dev/pompey/securite/Securite.java
@@ -8,24 +8,38 @@ import com.password4j.SaltGenerator;
 import static com.password4j.Password.check;
 
 public class Securite {
+    private static final String salt = "salt";
+//    Pour plus d'info : https://github.com/Password4j/password4j
+//    hashPassword("password", hash)
+    /**
+     * Hash le mot de passe
+     * @param password le mot de passe à hasher
+     * @return le hash du mot de passe
+     */
     public static String hashPassword(String password) {
         try {
             Hash hash = Password.hash(password)
-                    .addSalt(SaltGenerator.generate())
-                    .addPepper(PoivreToken.POIVRE)
-                    .withBcrypt();
+                        .addRandomSalt()
+                        .addPepper(PoivreToken.POIVRE)
+                        .withArgon2();
 
             return hash.getResult();
         }catch (Exception e){
-            throw new RuntimeException("Erreur lors du hashage du mot de passe");
+            throw new RuntimeException("Erreur lors du hashage du mot de passe", e);
         }
-
     }
-//TODO A FAIRE
+//    checkPassword("password", hash)
+    /**
+     * Vérifie si le mot de passe correspond au hash
+     * @param password le mot de passe à vérifier
+     * @param hash le hash à vérifier
+     * @return true si le mot de passe correspond au hash, false sinon
+     */
     public static boolean checkPassword(String password, String hash) {
         try {
-            HashChecker hashChecker = check(password, hash);
-            return hashChecker.check();
+            return Password.check(password, hash)
+                    .addPepper(PoivreToken.POIVRE)
+                    .withArgon2();
         }catch (Exception e){
             throw new RuntimeException("Erreur lors de la vérification du mot de passe");
         }
