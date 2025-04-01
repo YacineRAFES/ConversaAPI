59c93583a33222b2625a66ed6496ce88f7ad6bed
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-04-01 11:51:09 +0200

    Modification après avoir mis en place le Config

--- src/main/java/fr/afpa/dev/pompey/conversaapi/securite/PoivreToken.java
+++ src/main/java/fr/afpa/dev/pompey/conversaapi/securite/PoivreToken.java
@@ -1,5 +1,7 @@
 package fr.afpa.dev.pompey.conversaapi.securite;
 
+import static fr.afpa.dev.pompey.conversaapi.utilitaires.Config.getSecretPepper;
+
 public class PoivreToken {
-    public static final String POIVRE = System.getenv("PEPPER_SECRET");
+    public static final String POIVRE = getSecretPepper();
 }
