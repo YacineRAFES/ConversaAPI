6dd9fa4eb34d98931dc228c9badf765bd5bab8a0
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-04-03 16:44:50 +0200

    Ajout les alertes de messages pour SendJSON

---
+++ src/main/java/fr/afpa/dev/pompey/conversaapi/utilitaires/AlertMsg.java
@@ -0,0 +1,8 @@
+package fr.afpa.dev.pompey.conversaapi.utilitaires;
+
+public class AlertMsg {
+    public AlertMsg() {}
+    public static final String ErreurIdentifiant = "InvalidCredentials";
+
+    //TODO: A FAIRE LES ALERTES DE MESSAGES
+}
