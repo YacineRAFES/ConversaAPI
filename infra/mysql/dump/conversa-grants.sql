9ab8fc1a186303b542b341d0c15ce016d1254636
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-04-16 08:16:47 +0200

    Modif du fichier sql grants

--- infra/mysql/dump/conversa-grants.sql
+++ infra/mysql/dump/conversa-grants.sql
@@ -1,5 +1,5 @@
 CREATE USER 'utilisateur'@'localhost' IDENTIFIED BY 'utilisateur123';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `conversa`.`utilisateur` TO `utilisateur`@`localhost`;
 GRANT SELECT, INSERT, UPDATE, DELETE ON `conversa`.`amis` TO `utilisateur`@`localhost`;
-GRANT DELETE ON `conversa`.`groupe_messages_prives` TO 'utilisateur'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `conversa`.`groupe_messages_prives` TO 'utilisateur'@'localhost';
 FLUSH PRIVILEGES;
