6d2960c7c177c2f837c2a2ec53b51f4308e5e54d
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-05-28 15:54:03 +0200

    Mise à jour de SaisieException avec le javadoc

--- src/main/java/fr/afpa/dev/pompey/conversaapi/exception/SaisieException.java
+++ src/main/java/fr/afpa/dev/pompey/conversaapi/exception/SaisieException.java
@@ -1,10 +1,20 @@
 package fr.afpa.dev.pompey.conversaapi.exception;
 
+/**
+ * Classe exception personnalisé pour les saisies
+ */
 public class SaisieException extends Exception {
+
+    /**
+     * Construction SaisieException sans message
+     */
     public SaisieException() {
 
     }
 
+    /**
+     * Construction SaisieException avec message
+     */
     public SaisieException(String message) {
         super(message);
     }
