4eb17826797a0311e2ba40acc4fa9952eba749e4
Yacine RAFES <134509065+YacineRAFES@users.noreply.github.com>
2025-05-28 16:36:54 +0200

    modif

--- src/main/java/fr/afpa/dev/pompey/conversaapi/utilitaires/Regex.java
+++ src/main/java/fr/afpa/dev/pompey/conversaapi/utilitaires/Regex.java
@@ -2,7 +2,6 @@ package fr.afpa.dev.pompey.conversaapi.utilitaires;
 
 public class Regex {
     private Regex() {}
-    public static String NAME = "^[a-zA-ZàáâäçèéêëìíîïñòóôöùúûüÀÁÂÄÇÈÉÊËÌÍÎÏÑÒÓÔÖÙÚÛÜ'\\-\\s]+$";
     public static String EMAIL = "^(?=.{1,64}@)[\\p{L}0-9\\+_-]+(\\.[\\p{L}0-9\\+_-]+)*@"
             + "[^-][\\p{L}0-9\\+-]+(\\.[\\p{L}0-9\\+-]+)*(\\.[\\p{L}]{2,})$";
     public static String PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
